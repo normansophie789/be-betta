fa6fb5ad5e3d5bffb34394e72b90481d2d9ec1cb
Helen I J Evans <helenijevans@gmail.com>
2020-01-27 14:20:38 +0000

    fixed render issue

--- js/entities/HUD.js
+++ js/entities/HUD.js
@@ -60,15 +60,15 @@ game.HUD.ScoreItem = me.Renderable.extend({
             me.state.change(me.state.PLAY);
         }
 
-        // only incrementing points if in play state
-        if (me.state.isCurrent(me.state.PLAY)) {
-            game.data.timer += dt;
+        // // only incrementing points if in play state
+        // if (me.state.isCurrent(me.state.PLAY)) {
+        //     game.data.timer += dt;
 
-            if (game.data.timer >= 80) {
-                game.data.score ++;
-                game.data.timer = 0
-            }
-        }
+        //     if (game.data.timer >= 80) {
+        //         game.data.score ++;
+        //         game.data.timer = 0
+        //     }
+        // }
 
         return false;
     },
@@ -77,6 +77,14 @@ game.HUD.ScoreItem = me.Renderable.extend({
      * draw the score
      */
     draw : function (renderer) {
+        game.data.timer ++;
+
+       //console.log(game.data.timer);
+
+        if (game.data.timer >= 10) {
+            game.data.score ++;
+            game.data.timer = 0
+        }
        
 		this.font.draw(renderer, "Score: " + game.data.score , me.game.viewport.width + (this.pos.x * -2), me.game.viewport.height + (this.pos.y * -5));
         //this.font.draw(renderer, "Points: " + game.data.score , this.pos.x,  this.pos.y);
